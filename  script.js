3ba155d9fff17d302641a283213386c84649dfb0
cxzrom <141775736+cxzrom@users.noreply.github.com>
2024-06-02 17:09:14 +0300

    Update  script.js

---  script.js
+++  script.js
@@ -1,12 +1,33 @@
 document.getElementById("addProductBtn").addEventListener("click", function() {
-    openCamera("Додати товар");
+    sendDataToGoogleScript("Додати товар");
 });
 
 document.getElementById("returnProductBtn").addEventListener("click", function() {
-    openCamera("Повернути товар");
+    sendDataToGoogleScript("Повернути товар");
 });
 
-function openCamera(action) {
-    // Код для відкриття камери
-    // Ви можете використовувати getUserMedia або інші API для цього
+function sendDataToGoogleScript(action) {
+    var url = 'https://script.google.com/macros/s/AKfycbzude3T6YqReIz3GJaab1pPGB2BGWc7NtOZQ-5zbrtY0wvFeMdxId3sXb59ohcBJg37/exec';
+    var data = {
+        action: action
+    };
+
+    var options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+
+    fetch(url, options)
+        .then(response => {
+            if (response.ok) {
+                console.log('Дані успішно відправлені на сервер');
+                // Тут можна додати код для відкриття камери, якщо потрібно
+            } else {
+                console.error('Помилка при відправленні даних на сервер:', response.status);
+            }
+        })
+        .catch(error => console.error('Помилка при відправленні даних:', error));
 }
